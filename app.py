0aad915b3e3600be3578cbf2c559e4ad4e74616a
jerome hou <a10523023@yuntech.org.tw>
2017-11-22 03:27:26 +0800

    make it better

--- app.py
+++ app.py
@@ -87,10 +87,10 @@ def handle_message(event):
         #line_bot_api.push_message(event.source.user_id, TemplateSendMessage(text='shit'))
         t = 'fuckyou'
         line_bot_api.reply_message(
-            event.reply_token, [
+            event.reply_token, 
                 TemplateSendMessage(text='fuckyou'),
-                TemplateSendMessage(text='fuckyou')
-            ]
+                #TemplateSendMessage(text='fuckyou')
+            
         )
     if event.message.text == "北部地區":
         carousel_template = TemplateSendMessage(
