19fd49d46c4cc41dbca05dff49a4c8278063ab19
jerome hou <a10523023@yuntech.org.tw>
2018-06-09 02:54:33 +0800

    make it better

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def handle_message(event):
     print("event.source.user_id:", event.source.user_id)
     
     fuck = event.message.text
-    print(selectData('雲林縣')
+    print(selectData('雲林縣'))
     
     
     if fuck == "profile":
